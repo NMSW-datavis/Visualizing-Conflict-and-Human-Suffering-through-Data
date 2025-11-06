af8869b4cdf908c0098ecdea6e02fdf6bc10dbdd
Wassim Fatnassi <62265879+justwsx@users.noreply.github.com>
2025-11-06 17:32:01 +0100

    Update CivilianFatalitiesHist.py

--- CivilianFatalitiesHist.py
+++ CivilianFatalitiesHist.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("number_of_reported_civilian_fatalities_by_country-year_as-of-17Oct2025_0.csv")
 
 # --- Aggregate fatalities by country and year (in case there are duplicates) ---
-fatalities_by_country_year = df.groupby(["country", "Year"])["Fatalities"].sum().reset_index()
+fatalities_by_country_year = df.groupby(["Country", "Year"])["Fatalities"].sum().reset_index()
 
 # --- Select a country (example: Afghanistan) ---
 country = "Afghanistan"
-country_data = fatalities_by_country_year[fatalities_by_country_year["country"] == country]
+country_data = fatalities_by_country_year[fatalities_by_country_year["Country"] == country]
 
 # --- Create the bar chart ---
 plt.figure(figsize=(8, 5))
@@ -22,6 +22,4 @@ plt.ylabel("Number of Reported Civilian Fatalities")
 plt.grid(axis="y", linestyle="--", alpha=0.5)
 plt.tight_layout()
 
-# --- Save the image ---
-plt.savefig("charts/histogram-pyramid.png", dpi=300)
 plt.show()
